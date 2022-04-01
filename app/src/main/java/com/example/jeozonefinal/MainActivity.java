c1f3438d815e400b331a99fb4a59fb5e4d1f578a
BruhMacaque <70497748+BruhMacaque@users.noreply.github.com>
2022-03-31 22:25:22 -0400

    1

--- app/src/main/java/com/example/jeozonefinal/MainActivity.java
+++ app/src/main/java/com/example/jeozonefinal/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.jeozonefinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<QuizStuff> quizArray;
     Random random;
     int currentScore = 0;
-    int questionCounter = 1;
+    int questionCounter = 0;
     int currentPos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,23 +90,62 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void showBottomSheet(){
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.score_sheet, (LinearLayout)findViewById(R.id.LLScore));
+        TextView score = bottomSheetView.findViewById(R.id.Score);
+        Button restartButton = bottomSheetView.findViewById(R.id.RestartBtn);
+        score.setText("Your score is \n" + currentScore + "/5");
+        restartButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                currentPos = random.nextInt(quizArray.size());
+                setDataToView(currentPos);
+                questionCounter = 0;
+                currentScore = 0;
+                bottomSheetDialog.dismiss();
+            }
+        });
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+    }
+
     private void setDataToView(int currentPos){
-        questionNumber.setText("Questions Attempted: " + questionCounter + "/10");
+        questionNumber.setText("Questions Attempted: " + questionCounter + "/5");
 
-        question.setText(quizArray.get(currentPos).getQuestion());
-        option1Btn.setText(quizArray.get(currentPos).getOption1());
-        option2Btn.setText(quizArray.get(currentPos).getOption2());
-        option3Btn.setText(quizArray.get(currentPos).getOption3());
-        option4Btn.setText(quizArray.get(currentPos).getOption4());
-
-
+        if(questionCounter == 5){
+            showBottomSheet();
+        }else{
+            question.setText(quizArray.get(currentPos).getQuestion());
+            option1Btn.setText(quizArray.get(currentPos).getOption1());
+            option2Btn.setText(quizArray.get(currentPos).getOption2());
+            option3Btn.setText(quizArray.get(currentPos).getOption3());
+            option4Btn.setText(quizArray.get(currentPos).getOption4());
+        }
 
     }
     private void getQuizQuestions(ArrayList<QuizStuff> quizStuffPassed) {
         quizStuffPassed.add(new QuizStuff("What is the capital of Canada?", "Ottawa", "London", "Ontario", "Vancouver", "Ottawa"));
         quizStuffPassed.add(new QuizStuff("What is the capital of Ireland?", "Edinburg", "Dublin", "London", "Cardiff", "Dublin"));
-        quizStuffPassed.add(new QuizStuff("Where is the Colosseum located?", "Spain", "Austria", "France", "Italy", "Italy"));
-        quizStuffPassed.add(new QuizStuff("What is the capital of great Britain?", "Edinburg", "Dublin", "London", "Cardiff", "London"));
-
+        quizStuffPassed.add(new QuizStuff("Where is Colosseum located?", "Spain", "Austria", "France", "Italy", "Italy"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Russia?", "Saint-Petersburg", "Novgorod", "Kiev", "Moscow", "Moscow"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of India?", "Delhi", "Chennai", "Kolkata", "Bengaluru", "Delhi"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Poland?", "Warszawa", "Kraków", "Gdańsk", "Łódź", "Warszawa"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Argentina?", "Buenos Aires", "Mendoza", "Córdoba", "Rosario", "Buenos Aires"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Vietnam?", "Haiphong", "Hanoi", "Ho Chi Minh", "Pleiku", "Hanoi"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Wales?", "Edinburg", "Dublin", "London", "Cardiff", "Cardiff"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Uganda?", "Kasese", "Lira", "Mbarara", "Kampala", "Kampala"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Denmark?", "Randers", "Copenhagen", "Herning", "Kolding", "Copenhagen"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Kazakhstan?", "Almaty", "Taraz", "Nur-Sultan", "Turkistan", "Nur-Sultan"));
+        quizStuffPassed.add(new QuizStuff("What is the capital of Belgium?", "Brussels", "Antwerp", "Leuven", "Charleroi", "Brussels"));
+        quizStuffPassed.add(new QuizStuff("Where is Mount Kilimanjaro located?", "Kenya", "Mozambique", "Tanzania", "Ethiopia", "Tanzania"));
+        quizStuffPassed.add(new QuizStuff("Where is Mount Everest located?", "Asia", "Europe", "Africa", "North America", "Asia"));
+        quizStuffPassed.add(new QuizStuff("What is Taj Mahal located?", "Afghanistan", "Pakistan", "India", "Nepal", "India"));
+        quizStuffPassed.add(new QuizStuff("Where is Grand Canyon located?", "Arizona", "Kansas", "Texas", "Nevada", "Arizona"));
+        quizStuffPassed.add(new QuizStuff("Where is Easter Island located?", "Argentina", "Chile", "Bolivia", "Colombia", "Chile"));
+        quizStuffPassed.add(new QuizStuff("Where is Rainbow Mountain (Vinicunca) located?", "Australia", "Brazil", "Peru", "Argentina", "Peru"));
+        quizStuffPassed.add(new QuizStuff("Where is Cat Island located?", "Vietnam", "Thailand", "Korea", "Japan", "France"));
     }
 }
